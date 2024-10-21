761053cd406d6c1d8fd61a06e4318d2f2554f8f6
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-21 16:08:52 +0200

    more modifications

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
+// just playin' a bit u know
+
+let titles = [
+    "Software Engineering Student",
+    "Junior Mobile App Dev",
+    "Cybersecurity Enthousiast",
+]
+
+const span = document.getElementById("demo");
 const slides = document.querySelectorAll(".certificates-slider img");
 let index = 0;
 let intervalId = null;
+var i = 0;
+
+function total() {
+    let j = 0;
+    let i = 0;
+
+    function typeTitle() {
+        if (j < titles.length) {
+            if (i < titles[j].length) {
+                span.innerHTML += titles[j].charAt(i);
+                i++;
+                setTimeout(typeTitle, 200);
+            } else {
+                setTimeout(() => {
+                    span.innerHTML = "";
+                    i = 0;
+                    j++;
+                    setTimeout(typeTitle, 500);
+                }, 500);
+            }
+        } else {
+            j = 0;
+            setTimeout(typeTitle, 500);
+        }
+    }
+
+    typeTitle();
+}
+
+document.addEventListener("DOMContentLoaded", total);
 
 // initializeSlider();
 
@@ -9,7 +48,6 @@ document.addEventListener("DOMContentLoaded", initializeSlider);
 function initializeSlider(){
     if(slides.length > 0){
         slides[index].classList.add("displaySlide");
-        intervalId = setInterval(nextSlide, 5000);
     }
 
 }
